8c5bf98103d403f0bbbe3dfb1188561dd4626104
上海数聚 <v_shanghaishuju@fosun.com>
2020-05-06 17:19:25 +0800

    提交

---
+++ project-learn/src/main/java/com/wp/common/websocket/TestWebsocket.java
@@ -0,0 +1,65 @@
+package com.wp.common.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/websocket")
+@Slf4j
+public class TestWebsocket {
+    private static Map<String, Session> clients = new ConcurrentHashMap<>();
+
+    /**
+     * 客户端连接
+     * @param session session
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        //将新用户存入在线的组
+        log.info("有新的客户端连接了: {}", session.getId());
+        clients.put(session.getId(), session);
+    }
+
+    /**
+     * 客户端关闭
+     * @param session session
+     */
+    @OnClose
+    public void onClose(Session session) {
+        log.info("有用户断开了, id为:{}", session.getId());
+        clients.remove(session.getId());
+    }
+
+    /**
+     * 发生错误
+     * @param throwable e
+     */
+    @OnError
+    public void onError(Throwable throwable) {
+        throwable.printStackTrace();
+    }
+
+    /**
+     * 收到客户端发来消息
+     * @param message  消息对象
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        log.info("服务端收到客户端发来的消息: {}", message);
+    }
+
+    /**
+     * 群发消息
+     * @param message 消息内容
+     */
+    public void sendAll(String message) {
+        for (Map.Entry<String, Session> sessionEntry : clients.entrySet()) {
+            sessionEntry.getValue().getAsyncRemote().sendText(message);
+        }
+    }
+}
