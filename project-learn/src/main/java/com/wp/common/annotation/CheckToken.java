f2194298bb635e5ee2226428179ca52ea3d836dc
上海数聚 <v_shanghaishuju@fosun.com>
2020-05-18 16:09:13 +0800

    提交

--- project-learn/src/main/java/com/wp/common/annotation/CheckToken.java
+++ project-learn/src/main/java/com/wp/common/annotation/CheckToken.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 是否需要校验token注解
+ */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CheckToken {
